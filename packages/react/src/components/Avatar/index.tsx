06b098e6843985d5442857b308ed2aa6400598d3
Scatenarafael <rafascatena@gmail.com>
2023-03-10 19:47:05 -0300

    first_commit

---
+++ packages/react/src/components/Avatar/index.tsx
@@ -0,0 +1,19 @@
+import { User } from 'phosphor-react'
+import React, { ComponentProps } from 'react'
+import { AvatarContainer, AvatarFallBack, AvatarImage } from './styles'
+
+export interface AvatarProps extends ComponentProps<typeof AvatarImage> {}
+
+export function Avatar(props: AvatarProps) {
+  return (
+    <AvatarContainer>
+      <AvatarImage {...props} />
+
+      <AvatarFallBack delayMs={600}>
+        <User />
+      </AvatarFallBack>
+    </AvatarContainer>
+  )
+}
+
+Avatar.displayName = 'Avatar'
